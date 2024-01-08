81a48997df538ecebbb5f7e607247f96e9af72cf
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2024-01-08 20:30:51 +0400

    Decoupled models returned for each clients

--- algorithms/federation_byz.py
+++ algorithms/federation_byz.py
@@ -131,7 +131,7 @@ class NoGuardΞByzantine():
         model = copy.deepcopy(model_struct)
         if not gset: return model, {}
 
-        model.load_state_dict(gset["model"].state_dict(), strict=True)
+        model.load_state_dict(copy.deepcopy(gset["model"].state_dict()), strict=True)
         model = model.to(device)
         ghatch = {}
 
@@ -168,6 +168,113 @@ class NoGuardΞByzantine():
 
 
 ##------------------------------------------------------------------------------
+
+class NoGuardΞByzantineDecopl():
+    """
+    Prototype class for Decoupled FedAvg (very similar to decentralized mathematically)
+        In this each client will recieve differnt set of parameters at end
+        of each round, since averaging weightage for models will vary for each
+        client based on closeness to different models on some metric space
+    In this simply each model is given equal weightage, this just implementation place holder
+
+    """
+
+    def __init__(self, cfg, id, model, device="cpu"):
+        self.id = id
+        self.device = device
+        self.byz_way = None
+        self.model_0th = copy.deepcopy(model)
+        self.cfg = cfg
+        self.byztn_cfg = cfg.byztn_cfg
+        self.defense_cfg = cfg.defense_cfg
+
+        self.aggregator_func = self.__plain_fedavg #override this to introduce methods
+        print("DEFENSE: None")
+
+        if len(self.byztn_cfg) != 0:
+            byz_clients = [int(b) for b in self.byztn_cfg["byztn_clients"]]
+            if id in byz_clients:
+                self.byz_way = globals()[self.byztn_cfg["byztn_method"]](cfg, model)
+                print("BYZ METHOD: ", self.byztn_cfg["byztn_method"])
+
+
+    #-------- Client methods ----------
+
+    # @instancemethod #Locals calculation to send to Global
+    def synopsize_local(self, zxs): #used at end of local round at each client
+        """ zxs: {"model", }
+        """
+        lset = {}
+        model = fedops.model_copier(zxs["model"])
+
+        if self.byz_way:
+            out_state = self.byz_way.modify(model.state_dict())
+        else:
+            out_state = model.state_dict()
+
+        model.load_state_dict(out_state)
+        lset["model"] = model
+
+        return lset
+
+    #-------- Shared methods ----------
+
+    # @instancemethod #process global info for local use
+    def desynopsize_local(self, gset, device=None, model_struct=None): #used at end of local round at each client
+        """ model_struct: torch nn.module object
+            gset: global aggregations {"model", }
+        """
+        model_struct = self.model_0th if not model_struct else model_struct
+        if not device: device = next(model_struct.parameters()).device
+
+        ## since no compression or sketching used
+        model = copy.deepcopy(model_struct)
+        if not gset: return model, {}
+
+        ## NOTE: each client can/will access its own state_dicts alone
+        ## general dict notion is for easier code design
+        state_cli = gset["model_states_cli"][f"client_{self.id}"]
+
+        model.load_state_dict(state_cli, strict=True)
+        model = model.to(device)
+        ghatch = {}
+
+        return model, ghatch
+
+
+    #-------- Server methods ----------
+
+    def __plain_fedavg(self, lsets):
+        local_states = []
+        for ls in lsets:
+            local_states.append(ls["model"].state_dict())
+        agg_states = fedops.global_average_statedict(local_states)
+
+        agg_states_cli = { f"client_{i}": copy.deepcopy(agg_states)
+                          for i in range(len(lsets))}
+        agg_states_cli.update({"client_G": copy.deepcopy(agg_states)}) #Global Model
+
+        info_dict = {"client_weights":[[1/len(lsets)]*len(lsets)]*len(lsets)}
+        return agg_states_cli, info_dict
+
+
+    # @instancemethod  #Global calculation to send to locals
+    def aggregate_globally(self, lsets, device=None): #used at begining of local round central
+        """ Return: aggregated stat
+        """
+        if not device: device = next(self.model_0th.parameters()).device
+        for ls in lsets: ls["model"].to(device) #for nn.module .cuda is both inplace and assignable
+
+        agg_states_cli, select_info = self.aggregator_func(lsets)
+
+        ## NOTE: typically each client will have access only to its model params
+        ## but returning entire dict for sake of easier code design
+        gset = {"model_states_cli": agg_states_cli, "client_select": select_info}
+        return gset
+
+
+##==============================================================================
+
 
 
 class KrumΞByzantine(NoGuardΞByzantine):
