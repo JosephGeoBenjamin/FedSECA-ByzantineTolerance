2b3a2dd70e3dc4b4f5c5f70dd0ae5bb953f5b409
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2024-02-27 21:17:41 +0400

    dirichlet implementation

--- utilities/fedUtils.py
+++ utilities/fedUtils.py
@@ -1,7 +1,7 @@
 import os
 import copy
 import torch
-
+import numpy as np
 
 def gpu_devices_generator():
     gpu_ids = os.environ.get('CUDA_VISIBLE_DEVICES')
@@ -29,3 +29,74 @@ def find_layerwise_weight_difference(m1, m2):
         layerwise[k] = er.item()
 
     return layerwise
+
+
+def create_dirichlet_federated_split( data_name_list, label_list,
+                            dirichlet_parameter: float = 1,
+                            num_clients: int = 10):
+    """ Adapted from https://github.com/google-research/federated/blob/master/utils/datasets/cifar10_dataset.py
+    """
+
+    uniq_classes = set(label_list)
+    num_classes = len(uniq_classes)
+    num_samples = len(data_name_list)
+    dataname_array = np.asarray(data_name_list)
+    labels_array = np.asarray(label_list) #ease of use
+
+    train_clients = {}
+    train_multinomial_vals = []
+    # Each client has a multinomial distribution over classes drawn from a
+    # Dirichlet.
+    for k in range(num_clients):
+        proportion = np.random.dirichlet(dirichlet_parameter *
+                                        np.ones(num_classes,))
+        train_multinomial_vals.append(proportion)
+        print(proportion)
+
+    train_multinomial_vals = np.array(train_multinomial_vals)
+
+    train_example_indices = []
+    xamples_at_label_c = {}
+    for c in uniq_classes:
+        train_label_c = np.where(labels_array == c)[0]
+        np.random.shuffle(train_label_c)
+        train_example_indices.append(train_label_c)
+        xamples_at_label_c[c] = label_list.count(c)
+
+    train_example_indices = np.asarray(train_example_indices)
+    train_client_samples = [[] for _ in range(num_clients)]
+    train_count = np.zeros(num_classes).astype(int)
+    train_examples_per_client = int(num_samples / num_clients)
+
+    for k in range(num_clients):
+        for i in range(train_examples_per_client):
+            sampled_label = np.argwhere(
+                    np.random.multinomial(1, train_multinomial_vals[k, :]) == 1)[0][0]
+            ### Safetynet
+            # while_counter =0
+            # while train_count[sampled_label] >  xamples_at_label_c[sampled_label]:
+            #     print(np.random.multinomial(1, train_multinomial_vals[k, :]))
+            #     sampled_label = np.argwhere(
+            #         np.random.multinomial(1, train_multinomial_vals[k, :]) == 1)[0][0]
+            #     while_counter+=1
+            #     if while_counter>1000: break
+            ##---
+            train_client_samples[k].append(
+                train_example_indices[sampled_label][train_count[sampled_label]])
+            train_count[sampled_label] += 1
+            if train_count[sampled_label] == xamples_at_label_c[sampled_label]:
+                train_multinomial_vals[:, sampled_label] = 0
+                train_multinomial_vals = (
+                    train_multinomial_vals /
+                    train_multinomial_vals.sum(axis=1)[:, None])
+
+    for k in range(num_clients):
+        client_name = k
+        x_train = dataname_array[np.array(train_client_samples[k])]
+        y_train = labels_array[np.array(
+            train_client_samples[k])].astype('int64').squeeze()
+        train_data = {'image': x_train.tolist(), 'label': y_train.tolist()}
+        train_clients[client_name] = train_data
+
+    print(train_clients)
+    return train_clients
