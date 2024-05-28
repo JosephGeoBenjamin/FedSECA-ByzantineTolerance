b7e21c3dfdfec1586a4a8d301d879a25231a100a
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2024-05-28 19:42:52 +0400

    add Convnext and DeiT support

--- algorithms/feature_extractor.py
+++ algorithms/feature_extractor.py
@@ -2,7 +2,7 @@ import os
 import torch
 import torchvision
 from torch import nn
-
+import timm
 from torchvision.models.resnet import BasicBlock, ResNet
 
 class ImageLayerNorm(nn.LayerNorm):
@@ -18,7 +18,6 @@ class ImageLayerNorm(nn.LayerNorm):
         return x
 
 
-
 def freeze_weights(model):
     print("Freezing Resnet weights ...")
     for param in model.parameters():
@@ -26,6 +25,7 @@ def freeze_weights(model):
 
     return model
 
+##------------------------------------------------------------------------------
 
 def load_BasicBackbone(arch, torch_pretrain= None, freeze= False):
     if torch_pretrain in ["DEFAULT", "IMAGENET-1K"]:
@@ -49,6 +49,7 @@ def load_BasicBackbone(arch, torch_pretrain= None, freeze= False):
     return backbone, outfeat_size
 
 
+##------------------------------------------------------------------------------
 
 
 def resnet9(**kwargs):
@@ -72,26 +73,26 @@ def load_ResnetBackbone(arch, torch_pretrain= None, freeze= False):
     print("Torch Pretrain Set to ...", torch_pretrain)
 
     ## Model loading
-    if arch == 'resnet18':
+    if arch == 'resnet18': #11.6M params
         backbone = torchvision.models.resnet18(norm_layer = norm_layer)
         bk_state = torchvision.models.resnet18(zero_init_residual=True,
                                     weights=torch_pretrain).state_dict()
         backbone.load_state_dict(bk_state, strict=False)
 
-        weights=torch_pretrain
         outfeat_size = 512
 
-    elif arch == 'resnet50':
+    elif arch == 'resnet50': #25.6M params
         backbone = torchvision.models.resnet50(norm_layer= norm_layer)
         bk_state = torchvision.models.resnet50(zero_init_residual=True,
-                            weights=torch_pretrain)
+                            weights=torch_pretrain).state_dict()
         backbone.load_state_dict(bk_state, strict=False)
 
         outfeat_size = 2048
 
-    elif arch == 'resnet9':
+    elif arch == 'resnet9': #6.5M params
         backbone  = resnet9(norm_layer= norm_layer)
         outfeat_size = 512
+        if torch_pretrain: Warning(f"{arch} has no imagenet pretraining available")
 
     else:
         raise ValueError(f"Unsupported Model Implementation {arch} called in {os.path.basename(__file__)}")
@@ -101,6 +102,9 @@ def load_ResnetBackbone(arch, torch_pretrain= None, freeze= False):
     if freeze: backbone = freeze_weights(backbone)
 
     return backbone, outfeat_size
+
+
+##------------------------------------------------------------------------------
 
 
 def load_EfficientnetBackbone(arch, torch_pretrain= None, freeze= False):
@@ -115,7 +119,7 @@ def load_EfficientnetBackbone(arch, torch_pretrain= None, freeze= False):
     print("Torch Pretrain Set to ...", torch_pretrain)
 
     ## Model loading
-    if arch == 'efficientnet_b0':
+    if arch == 'efficientnet_b0': #5.3M param
         backbone = torchvision.models.efficientnet_b0(zero_init_residual=True,
                                 weights=torch_pretrain)
         outfeat_size = 1280
@@ -128,37 +132,93 @@ def load_EfficientnetBackbone(arch, torch_pretrain= None, freeze= False):
 
     return backbone, outfeat_size
 
+##------------------------------------------------------------------------------
 
 def load_ConvNextBackbone(arch, torch_pretrain= None, freeze= False):
 
     ## pretrain setting
     if torch_pretrain in ["DEFAULT", "IMAGENET-1K"]:
-        torch_pretrain = "DEFAULT"
+        torch_pretrain = True
     elif torch_pretrain in [None, "NONE", "none", "None"]:
-        torch_pretrain = None
+        torch_pretrain = False
     else:
         raise ValueError("Unknown pretrain weight type requested ", torch_pretrain )
     print("Torch Pretrain Set to ...", torch_pretrain)
 
     ## Model loading
-    if arch == 'convnext_tiny':
-        backbone = torchvision.models.convnext_tiny(
-                                weights=torch_pretrain)
+
+    # backbone = torchvision.models.convnext_tiny(weights=torch_pretrain) # pytorch
+    if arch == 'convnext_base': #89M param
+        model_card_name = "timm/convnext_base.fb_in1k"
+        outfeat_size = 1024
+
+    elif arch == 'convnext_tiny': #28.6M param
+        model_card_name = "timm/convnext_tiny.fb_in1k"
         outfeat_size = 768
-    elif arch == 'convnext_small':
-        backbone = torchvision.models.convnext_small(
-                                weights=torch_pretrain)
-        outfeat_size = 768
+
+    elif arch == 'convnext_nano':#15.6M param
+        model_card_name = "timm/convnext_nano_ols.d1h_in1k"
+        outfeat_size = 640
+
     else:
         raise ValueError(f"Unsupported Model Implementation {arch} called in {os.path.basename(__file__)}")
-    backbone.classifier = nn.Flatten(start_dim=1)  #remove fc of default arch
+
+    print("Loading timm Model Architecture....", model_card_name)
+
+    backbone = timm.create_model(model_card_name,
+                                pretrained=torch_pretrain,   # num_classes=0,
+                                )
+    backbone.head = nn.Sequential(nn.AdaptiveAvgPool2d((1,1)), nn.Flatten())
+
+    # backbone = torchvision.models.convnext_tiny(weights=torch_pretrain)
 
     # freeze model
     if freeze: backbone = freeze_weights(backbone)
 
     return backbone, outfeat_size
 
+##------------------------------------------------------------------------------
 
+
+def load_DeitBackbone(arch, torch_pretrain= None, freeze= False):
+    ## pretrain setting
+    if torch_pretrain in ["DEFAULT", "IMAGENET-1K"]:
+        torch_pretrain = True
+    elif torch_pretrain in [None, "NONE", "none", "None"]:
+        torch_pretrain = False
+    else:
+        raise ValueError("Unknown pretrain weight type requested ", torch_pretrain )
+    print("Torch Pretrain Set to ...", torch_pretrain)
+
+    ## Model loading
+    if arch == "deit_tiny":  #5.7M param
+        model_card_name = 'timm/deit_tiny_patch16_224.fb_in1k'
+        outfeat_size = 192
+
+    if arch == "deit_small": #22.1M param
+        model_card_name = 'timm/deit_small_patch16_224.fb_in1k'
+        outfeat_size = 384
+
+    if arch == "deit_base":  #86.6M param
+        model_card_name = 'timm/deit_base_patch16_224.fb_in1k'
+        outfeat_size = 768
+
+    else:
+        raise ValueError(f"Unsupported Model Implementation {arch} called in {os.path.basename(__file__)}")
+    print("Loading timm Model Architecture....", model_card_name)
+
+    backbone = timm.create_model(model_card_name,
+                                 pretrained=torch_pretrain,
+                                # num_classes=0,  # remove classifier nn.Linear
+                                )
+    raise "Classifier Not set"
+    # freeze model
+    if freeze: backbone = freeze_weights(backbone)
+
+    return backbone,
+
+
+##==============================================================================
 
 def getBackboneNetwork(arch, torch_pretrain= None, freeze= False):
 
@@ -168,6 +228,8 @@ def getBackboneNetwork(arch, torch_pretrain= None, freeze= False):
         network = load_EfficientnetBackbone(arch, torch_pretrain, freeze)
     elif "convnext" in arch:
         network = load_ConvNextBackbone(arch, torch_pretrain, freeze)
+    elif "deit" in arch:
+        network = load_DeitBackbone(arch, torch_pretrain, freeze)
     elif "basic" in arch:
         network = load_BasicBackbone(arch, torch_pretrain, freeze)
     else:
